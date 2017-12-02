b2693bba291c0b0f5602968b5fdc9086e6dfa423
Thomas Bowlin <thomasjbowlin94@gmail.com>
2017-12-02 14:56:36 -0500

    Add print_usage() when no arguments are provided

--- hostd.c
+++ hostd.c
@@ -14,6 +14,26 @@
 
 int main(int argc, char* argv[])
 {
-    printf("Hello world!\n");
+    char* input_file;
+    FILE* input_list_stream;
+    struct pcb* input_queue = NULL;
+    struct_pcb* current_process = NULL;
+    struct_pcb* process = NULL;
+    int timer = 0;
+
+    if (argc == 2)
+    {
+        input_file = argv[1];
+    }
+    else
+    {
+        print_usage()
+    }
+
     return 0;
+}
+
+void print_usage()
+{
+    printf("Please supply a file specifying a sequence of processes. as an argument when running this file.");
 }
--- hostd.h
+++ hostd.h
@@ -20,3 +20,4 @@
 
 #include "process.h"
 
+void print_usage();
