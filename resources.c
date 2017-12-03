4ee4ed6b28c93b04a19af9bce9dd6511ebb5271b
Thomas Bowlin <thomasjbowlin94@gmail.com>
2017-12-03 12:01:06 -0500

    Thouroughly comment resources module

--- resources.c
+++ resources.c
@@ -4,24 +4,45 @@
 **      date:       11/28/2017
 **      class:      CSC400, Fall 2017 - Dr. Zhang
 **
-**      file description: 
+**      file description: resources.c
+**      --------------------------------
+**      Source file for the resources module. Contains function definitions for all behavior
+**      needed to manage the resources of a system. 
 **
 */
 
 
+// Includes
 #include "resources.h"
 
 
+/*
+** Function: create_null_resources()
+** ---------------------------------
+** Creates a new pointer to resources struct with all values initialized to 0
+**
+** in: void
+** out: struct resources*
+*/
 struct resources* create_null_resources()
 {
-    struct resources* rsrcs = (struct resources*)malloc(sizeof(struct resources));
-    rsrcs->remaining_printers = 0;
+    struct resources* rsrcs = (struct resources*)malloc(sizeof(struct resources));  // Create pointer to new struct
+    rsrcs->remaining_printers = 0;  // Initialize all values to 0
     rsrcs->remaining_scanners = 0;
     rsrcs->remaining_modems = 0;
     rsrcs->remaining_drives = 0;
     return rsrcs;
 }
 
+/*
+** Function: check_resources()
+** ---------------------------------
+** Checks the resources required by a process against the resources left in the system
+** to determine whether there are sufficient resources available to complete the process
+**
+** in: struct pcb*, struct resources*
+** out: bool
+*/
 bool check_resources(struct pcb* p, struct resources* rsrcs)
 {
     return (rsrcs->remaining_printers >= p->num_printers && 
@@ -30,6 +51,15 @@ bool check_resources(struct pcb* p, struct resources* rsrcs)
         rsrcs->remaining_drives >= p->num_drives);
 }
 
+/*
+** Function: allocate_resources()
+** ---------------------------------
+** Allocates the resources required by a particular process to that process by subtracting
+** the corresponding number of resources from the count of the system.
+**
+** in: struct pcb*, struct resources*
+** out: void
+*/
 void allocate_resources(struct pcb* p, struct resources* rsrcs)
 {
     rsrcs->remaining_printers -= p->num_printers;
@@ -38,6 +68,15 @@ void allocate_resources(struct pcb* p, struct resources* rsrcs)
     rsrcs->remaining_drives -= p->num_drives;
 }
 
+/*
+** Function: free_resources()
+** ---------------------------------
+** Frees the resources required by a particular process from that process by adding
+** the corresponding number of resources back into the count of the system
+**
+** in: struct pcb*, struct resources*
+** out: void
+*/
 void free_resources(struct pcb* p, struct resources* rsrcs)
 {
     rsrcs->remaining_printers += p->num_printers;
--- resources.h
+++ resources.h
@@ -4,24 +4,34 @@
 **      date:       11/28/2017
 **      class:      CSC400, Fall 2017 - Dr. Zhang
 **
-**      file description: 
+**      file description: resources.h
+**      --------------------------------
+**      Header file for the resources module. Contains the definition for the resources struct,
+**      as well as declarations for all functions used in the resources module. Function definitions,
+**      and more extensive comments can be found in the resources.c file
 **
 */
 
 
+// Includes
 #include <stdbool.h>
 #include "process.h"
 
 
+/*
+**  The resources struct represents a collection of I/O resources contained by a system. As
+**  resources are used by a process, the count of the respective resources is decremented. When 
+** a process is finished, the count is incremented.
+*/
 struct resources
 {
-    unsigned int remaining_printers;
-    unsigned int remaining_scanners;
-    unsigned int remaining_modems;
-    unsigned int remaining_drives;  
+    unsigned int remaining_printers;    // The number of remaining printers within the system
+    unsigned int remaining_scanners;    // The number of remaining scanners within the system
+    unsigned int remaining_modems;  // The number of remaining modems within the system
+    unsigned int remaining_drives;  // The number of remaining CD drives within the system
 };
 
-struct resources* create_null_resources();
-bool check_resources(struct pcb* p, struct resources* rsrcs);
-void allocate_resources(struct pcb* p, struct resources* rsrcs);
-void free_resources(struct pcb* p, struct resources* rsrcs);
+struct resources* create_null_resources();  // Create a pointer to a 0-initialized resources struct
+bool check_resources(struct pcb* p, struct resources* rsrcs);   // Check that a system has enough resources to work on a process
+void allocate_resources(struct pcb* p, struct resources* rsrcs);    // Allocate resources to a particular process
+void free_resources(struct pcb* p, struct resources* rsrcs);    // Free the resources used by a particular process
