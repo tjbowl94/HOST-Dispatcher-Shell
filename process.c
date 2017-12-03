a3664e7b1bf914002c1d5bb5fb6638650817ef5a
Thomas Bowlin <thomasjbowlin94@gmail.com>
2017-12-03 14:11:24 -0500

    Thouroughly comment process module

--- process.c
+++ process.c
@@ -4,18 +4,30 @@
 **      date:       11/28/2017
 **      class:      CSC400, Fall 2017 - Dr. Zhang
 **
-**      file description: 
+**      file description: process.c
+**      -----------------------------
+**      Source file for the process module. Contains function definitions for all
+**      behavior needed for process management
 **
 */
 
 
+// Includes
 #include "process.h"
 
 
+/*
+** Function: create_null_pcb()
+** ---------------------------------
+** Creates a new pointer to pcb struct with all values initialized to NULL/0 or default values
+**
+** in: void
+** out: struct pcb* - pointer to newly created pcb
+*/
 struct pcb* create_null_pcb()
 {
-    struct pcb* p = (struct pcb*)malloc(sizeof(struct pcb));
-    p->pid = 0;
+    struct pcb* p = (struct pcb*)malloc(sizeof(struct pcb));    // Create new pcb
+    p->pid = 0; // Initialize all values to 0/NULL/default values
     p->args[0] = DEFAULT_PROCESS;
     p->args[1] = NULL;
     p->arrival_time = 0;
@@ -33,17 +45,26 @@ struct pcb* create_null_pcb()
     return p;
 }
 
+/*
+** Function: start_pcb()
+** ---------------------------------
+** Starts a process for the first time by forking and running the process. Additionally
+** updates the status of the pcb
+**
+** in: struct pcb* - pointer to process to be started
+** out: struct pcb* - pointer to process newly started
+*/
 struct pcb* start_pcb(struct pcb* p)
 {
-    if (p->pid == 0)
+    if (p->pid == 0)    // Make sure process has not been started already
     {
-        switch(p->pid = fork())
+        switch(p->pid = fork()) // Start the process by forking
         {
-            case -1:
+            case -1:    // Handle error in the case process did not fork correctly
                 // Handle error
                 exit(1);
-            case 0:
-                p->pid = getpid();
+            case 0: 
+                p->pid = getpid();  // Otherwise, update pcb
                 p->status = PCB_RUNNING;
                 // Handle printing running status
                 fflush(stdout);
@@ -51,7 +72,7 @@ struct pcb* start_pcb(struct pcb* p)
                 exit(2);
         }
     }
-    else
+    else    // If process was already started, just restart it by sending continue signal
     {
         kill(p->pid, SIGCONT);
     }
@@ -59,58 +80,103 @@ struct pcb* start_pcb(struct pcb* p)
     return p;
 }
 
+
+/*
+** Function: suspend_pcb()
+** ---------------------------------
+** Suspends a process that had previously been started already.
+**
+** in: struct pcb* - pointer to running process
+** out: struct pcb* - pointer to newly suspended process
+*/
 struct pcb* suspend_pcb(struct pcb* p)
 {
     int status;
-    kill(p->pid, SIGTSTP);
-    waitpid(p->pid, &status, WUNTRACED);
-    p->status = PCB_SUSPENDED;
+    kill(p->pid, SIGTSTP);  // Send a stop signal to process
+    waitpid(p->pid, &status, WUNTRACED);    // Wait for return from process
+    p->status = PCB_SUSPENDED;  // Update pcb status
     return p;
 }
 
+
+/*
+** Function: restart_pcb()
+** ---------------------------------
+** Restarts a process that was previously suspended.
+**
+** in: vstruct pcb* - pointer to process to be suspended
+** out: struct pcb* - pointer to newly suspended process
+*/
 struct pcb* restart_pcb(struct pcb* p)
 {
-    kill(p->pid, SIGCONT);
+    kill(p->pid, SIGCONT);  // Send a continue signal to process
     return p;
 }
 
+
+/*
+** Function: terminate_pcb()
+** ---------------------------------
+** Terminates a process. To be called when proccess is finished running.
+**
+** in: struct pcb* - pointer to process to be terminated
+** out: struct pcb* - pointer to newly terminated process
+*/
 struct pcb* terminate_pcb(struct pcb* p)
 {
     int status;
-    kill(p->pid, SIGINT);
-    waitpid(p->pid, &status, WUNTRACED);
-    p->status = PCB_TERMINATED;
+    kill(p->pid, SIGINT);   // Send kill signal to process
+    waitpid(p->pid, &status, WUNTRACED);    // Wait for response from process
+    p->status = PCB_TERMINATED; // Update status to terminated
     return p;
 }
 
+
+/*
+** Function: enqueue_pcb()
+** ---------------------------------
+** Adds a process to the back of a queue
+**
+** in: struct pcb* - pointer to head of queue, struct pcb* - pointer to process to be added to queue
+** out: struct pcb* - pointer to head of updated queue
+*/
 struct pcb* enqueue_pcb(struct pcb* head, struct pcb* p)
 {
     struct pcb* curPcb = head;
-    if (head)
+    if (head)   // If the queue is not currently empty
     {
-        while (curPcb->next)
+        while (curPcb->next)    // Find the end of the queue
         {
-            curPcb = curPcb->next;
+            curPcb = curPcb->next;  
         }
-        curPcb->next = p;
+        curPcb->next = p;   // Add the process to the end of queue
         return head;
     }
-    else
+    else    // If the queue is empty, just return p
     {
         return p;
     }
 }
 
+
+/*
+** Function: dequeue_pcb()
+** ---------------------------------
+** Removes a process from the front of a queue.
+**
+** in: struct pcb* - head of queue 
+** out: struct pcb* - new head of queue, with previous head removed
+*/
 struct pcb* dequeue_pcb(struct pcb* head)
 {
     if (!head)
     {
-	return NULL;
+	    return NULL;    // Return NULL if queue is empty
     }
     else
     {
-    	struct pcb* newHead = head->next;
-	head->next = NULL;
-	return newHead;
+        struct pcb* newHead = head->next;   // Otherwise remove head 
+	    head->next = NULL;
+	    return newHead; // Return new head
     }
 }
--- process.h
+++ process.h
@@ -4,11 +4,17 @@
 **      date:       11/28/2017
 **      class:      CSC400, Fall 2017 - Dr. Zhang
 **
-**      file description: 
+**      file description: process.h
+**      ----------------------------
+**      Header file for the process module. Contains definition of useful constants,
+**      definition for the pcb struct, as well as function declarations for the 
+**      functions used to manage processes. Function definitions and more extensive 
+**      comments can be found in the process.c file
 **
 */
 
 
+// Includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
@@ -18,10 +24,13 @@
 #include "memory.h"
 
 
+// The argument size for a particular process
 #define MAX_ARGS            3
 
+// The process that will run for each new process (source code in sigtrap.c)
 #define DEFAULT_PROCESS     "./process"
 
+// Status codes for each process
 #define PCB_UNINITIALIZED   0
 #define PCB_INITIALIZED     1
 #define PCB_READY           2
@@ -29,31 +38,37 @@
 #define PCB_SUSPENDED       4
 #define PCB_TERMINATED      5
 
+/*
+** The pcb struct represents the process control block for each new process. It contains a collection
+** of data representing the control block, including process id, arrival time, priority, remaining cpu time,
+** status, number of required resources, pointer to memory block, and pointer to next pcb (if the process)
+** is currently in a queue
+*/
 struct pcb
 {
-    pid_t pid;                      
-    char* args[MAX_ARGS];        
-    unsigned int arrival_time;
-    unsigned int priority;
-    unsigned int remaining_cpu_time;
-    unsigned int status;
+    pid_t pid;  // Process id 
+    char* args[MAX_ARGS];   // Process arguments    
+    unsigned int arrival_time;  // Time process arrives in queue
+    unsigned int priority;  // Priority of process (0 for real-time, 1-3 for user job)
+    unsigned int remaining_cpu_time;    // Time left until process is completed
+    unsigned int status;    // Current status of process
 
-    unsigned int num_printers;
-    unsigned int num_scanners;
-    unsigned int num_modems;
-    unsigned int num_drives;
-    unsigned int mbytes;
+    unsigned int num_printers;  // Number of printers required by process
+    unsigned int num_scanners;  // Number of scanners required by process
+    unsigned int num_modems;    // Number of modems required by process
+    unsigned int num_drives;    // Number of drives required by process
+    unsigned int mbytes;    // Memory requirement of process in mbytes
 
-    struct mab* mem_block;
+    struct mab* mem_block;  // Pointer to memory block for process (if it has been allocated)
 
-    struct pcb* next;
+    struct pcb* next;   // Pointer to next process (if in queue)
 };
 
-struct pcb* create_null_pcb();
-struct pcb* start_pcb(struct pcb* p);
-struct pcb* suspend_pcb(struct pcb* p);
-struct pcb* restart_pcb(struct pcb* p);
-struct pcb* terminate_pcb(struct pcb* p);
+struct pcb* create_null_pcb();  // Creates a pointer to new pcb with all values initialized to null/0
+struct pcb* start_pcb(struct pcb* p);   // Starts a process
+struct pcb* suspend_pcb(struct pcb* p); // Suspends a process
+struct pcb* restart_pcb(struct pcb* p); // Restarts a previously-suspended process
+struct pcb* terminate_pcb(struct pcb* p);   // Terminates a process
 
-struct pcb* enqueue_pcb(struct pcb* head, struct pcb* p);
-struct pcb* dequeue_pcb(struct pcb* head);
+struct pcb* enqueue_pcb(struct pcb* head, struct pcb* p);   // Adds a process to a queue
+struct pcb* dequeue_pcb(struct pcb* head);  // Removes the front of a process queue 
